1b887209b848bc9e08cb4c96fc37c9ea1384406f
nnyong <jo.youthful@gmail.com>
2019-04-09 08:44:01 +0000

    190409 | Insta - Image upload

--- django/insta/insta/settings.py
+++ django/insta/insta/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'imagekit',
     # 'posts.app.PostsConfig', 옛날 버전!
     'posts', # 요즘버전!
 ]
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -9,7 +9,7 @@ def list(request):
 
 def create(request):
     if request.method=='POST':
-        post_form=PostForm(request.POST)
+        post_form=PostForm(request.POST,request.FILES)
         if post_form.is_valid():
             post_form.save()
             return redirect('posts:list')
@@ -17,6 +17,17 @@ def create(request):
         post_form=PostForm()
     return render(request, 'posts/create.html',{'post_form':post_form})
     
+def update(request,post_id):
+    post=get_object_or_404(Post,id=post_id)
+    if request.method=='POST':
+        post_form=PostForm(request.POST,request.FILES,instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('posts:list')
+    else:
+        post_form=PostForm(instance=post)
+    return render(request,'posts/create.html',{'post_form':post_form})
+
 def delete(request,post_id):
     # post=Post.objects.get(id=post_id)
     post=get_object_or_404(Post,id=post_id)
